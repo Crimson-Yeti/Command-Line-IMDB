33a947a7d40701ca3b57463f45a95ab1ff8e6434
Bryce-Tucker <bryce.tucker.0598@gmail.com>
2022-02-10 01:29:56 -0700

    v2.0.0 - Updated program to work with IMDB site changes

--- cmd_imdb.py
+++ cmd_imdb.py
@@ -4,13 +4,20 @@
 import re, textwrap, sys
 import requests, bs4 # Third-Party Modules
 
+MONTHS = {
+    "January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "August": 8,
+    "September": 9, "October": 10, "November": 11, "December": 12
+}
+
+
 def main():
     link = search_imdb(get_user_input())
     if not link: quit()
     movie_info = imdb_get_all_info(link)
     line_width = 150
+    print(movie_info)
     print('\n' + '=' * line_width)
-    print (movie_info['title'].ljust(line_width//3) + movie_info['type'].center(line_width//3) +  movie_info['date'].rjust(line_width//3))
+    print (movie_info['title'].ljust(line_width//3) + movie_info['type'].center(line_width//3) + str(movie_info['date']['Y']).rjust(line_width//3))
     print (("IMDB Rating: " + movie_info['rating']).ljust(line_width//4) + movie_info['parental_rating'].center(line_width//4) +
             str(movie_info['genres']).center(line_width//4) + movie_info['runtime'].rjust(line_width//4))
     print('=' * line_width)
@@ -18,13 +25,11 @@ def main():
     print(textwrap.fill(movie_info['summary'], line_width))
 
 
-
 def get_user_input():                   # Returns a string containing user provided movie/tv title
     if len(sys.argv) > 1:               # Checks if title was provided while calling the program
         return " ".join(sys.argv[1:])       # Returns any command line arguments following the program call as a space seperated string
     else:                               # If the tile wasn't given while calling the program, the program prompts the user for input and returns the result
         return input("Enter the title of the movie or show you want to search for: ")
-
 
 
 def search_imdb(title, field='Titles', choice='m'):       # Shows search results for given title and returns the link to user's selection
@@ -59,13 +64,11 @@ def search_imdb(title, field='Titles', choice='m'):       # Shows search results
         return None
 
 
-
 def get_soup(link, headers={'User-Agent': 'My User Agent 1.0'}):
     page = requests.get(link, headers=headers)
     page.raise_for_status()
     soup = bs4.BeautifulSoup(page.text, 'lxml')
     return soup
-
 
 
 def imdb_go_to_page(imdb_tag, page_type):
@@ -79,6 +82,7 @@ def imdb_go_to_page(imdb_tag, page_type):
             return "https://www.imdb.com/title/{}/trivia".format(imdb_tag)
         elif imdb_tag[0:2] == 'nm':
             return "https://www.imdb.com/name/{}/bio".format(imdb_tag)
+
 
 def imdb_get_all_info(link, movie_dict={}):
     imdb_soup = get_soup(link)
@@ -94,56 +98,86 @@ def imdb_get_all_info(link, movie_dict={}):
 
     return movie_dict
 
+
 def imdb_get_title(imdb_soup):
-    title = imdb_soup.find('div', class_='title_wrapper')
-    if title:
-        title = title.h1
-        if title.span: title.span.decompose()
-    return None if not title else title.text
+    title = imdb_soup.find('h1', {"data-testid":"hero-title-block__title"})
+    print(title.text.strip())
+    return None if not title else title.text.strip()
+
 
 def imdb_get_parental_rating(imdb_soup):
-    subtext = imdb_soup.find('div', class_='subtext')
-    return None if not subtext else subtext.text.split('|')[0].strip()
+    subtext = imdb_soup.findAll('span', {"class":"jedhex"})
+    if len(subtext) < 2:
+        return 'None'
+    subtext = subtext[1]
+    print(subtext.text.strip())
+    return 'None' if not subtext else subtext.text.strip()
+
+
+def convert_date(date):
+    date = date.replace(',', '')
+    df = date.split(" ")
+    return {
+        "M": MONTHS[df[0]],
+        "D": int(df[1]),
+        "Y": int(df[2])
+    }
+
 
 def imdb_get_date(imdb_soup):
-    date_regex = re.compile(r'''\d(\d)? \w* \d{4}''')
-    tv_series_date_regex = re.compile(r'''(\(\d{4}–( )?(\d\d\d\d)?\))''')
-    info = imdb_soup.find('a', title='See more release dates')
-    if info:
-        if date_regex.search(info.text):
-            return date_regex.search(info.text).group()
-        elif tv_series_date_regex.search(info.text):
-            return tv_series_date_regex.search(info.text).group()
+    dateRegex = re.compile(r'''(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2},\s+\d{4}''')
+    links = imdb_soup.findAll('a', {"class": "ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link"})
+    for link in links:
+        if dateRegex.search(link.text):
+            print(convert_date(dateRegex.search(link.text).group()))
+            return convert_date(dateRegex.search(link.text).group())
     return None
+
 
 def imdb_get_genres(imdb_soup):
-    genre_text = imdb_soup.find('div', class_='subtext')
-    return None if not genre_text else genre_text.text.split('|')[2].strip().replace(', \n', ' ').split()
+    genres = []
+    texts = imdb_soup.findAll('a', {"class": "LKJMs"})
+    for text in texts:
+        genres.append(text.text.strip())
+    print(genres)
+    return None if len(genres) == 0 else genres
+
 
 def imdb_get_rating(imdb_soup):
-    rating_value = imdb_soup.find('span', itemprop='ratingValue')
+    rating_value = imdb_soup.find('span', {"class": "iTLWoV"})
+    print(rating_value.text.strip())
     return None if not rating_value else rating_value.text.strip()
 
+
 def imdb_get_summary(imdb_soup):
-    summary_text = imdb_soup.find('div', class_='summary_text')
-    if summary_text.find('a', href=True):
-        full_summary_soup = get_soup('http://www.imdb.com' + summary_text.find('a', href=True)['href'])
-        summary_text = full_summary_soup.find('ul', id='plot-summaries-content').li.p
+    summary_text = imdb_soup.find('span', {"data-testid":"plot-xl"})
     return None if not summary_text else summary_text.text.strip()
 
+
 def imdb_get_runtime(imdb_soup):
-    runtime_text = imdb_soup.find('time', datetime=True)
-    return None if not runtime_text else runtime_text.text.strip()
+    rtRegex = re.compile(r'''(\d+h )?\d+m''')
+    lis = imdb_soup.findAll('li', {"class": "ipc-inline-list__item"})
+    for li in lis:
+        if rtRegex.search(li.text):
+            print(rtRegex.search(li.text).group())
+            return rtRegex.search(li.text).group()
+    return None
+
 
 def imdb_get_type(imdb_soup):
-    types = ['TV Series', 'TV Movie', 'Video', 'Short']
-    result_info_text = imdb_soup.find('a', title='See more release dates')
-    if result_info_text:
-        for type in types:
-            if type in result_info_text.text: return type
-        if 'Episode aired' in result_info_text.text: return 'TV Episode'
-        else: return 'Movie'
-    return None
+    types = ['TV Series', 'TV Mini Series', 'TV Movie', 'TV Short', 'Short', 'Video', 'Movie']
+    type = 'Movie'
+    lis = imdb_soup.findAll('li', {"class":"ipc-inline-list__item"})
+    for li in lis:
+        if li.text.strip() in types:
+            if type != 'Movie':
+                if 'Short' in type and 'Short' in li.text.strip():
+                    continue
+                type += " " + li.text.strip()
+            else:
+                type = li.text.strip()
+    print(type)
+    return type
 
 
 if __name__ == '__main__':
